e78b87c770e424758e774d99593383c385dd0839
Ronnie Song <ronniesong0809@gmail.com>
2023-02-26 20:26:18 -0800

    feat: add CORS

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from services import get_answer, get_answer_v2, generate_qa
 from utils.fetch import fetch_data
 from dotenv import load_dotenv
@@ -7,6 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/ama", methods=["GET"])
